7172932c3852e30167d81b4e0eb680f453f6ae68
PJ <1255257582@qq.com>
2023-07-10 12:04:44 +0800

    汉堡王

---
+++ src/components/UI/Mask.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(["isShow"])
+const emits = defineEmits(["hide"])
+</script>
+
+<template>
+    <Teleport to="body">
+        <div :="$attrs" 
+            @click.self="$emit('hide')" 
+            class="mask" v-show="props.isShow">
+            <slot></slot>
+        </div>
+    </Teleport>
+    
+</template>
+
+<style scoped>
+.mask {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+}
+</style>
